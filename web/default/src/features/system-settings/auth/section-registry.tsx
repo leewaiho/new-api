b058d0a021fad1e2856c3fce2a366fced51431f0
weihao.li <791783391@qq.com>
2026-07-12 01:41:32 +0800

    feat: configure dashboard session lifetime

--- web/default/src/features/system-settings/auth/section-registry.tsx
+++ web/default/src/features/system-settings/auth/section-registry.tsx
@@ -32,6 +32,7 @@ const AUTH_SECTIONS = [
       <BasicAuthSection
         defaultValues={{
           PasswordLoginEnabled: settings.PasswordLoginEnabled,
+          DashboardSessionLifetimeDays: settings.DashboardSessionLifetimeDays,
           PasswordRegisterEnabled: settings.PasswordRegisterEnabled,
           EmailVerificationEnabled: settings.EmailVerificationEnabled,
           RegisterEnabled: settings.RegisterEnabled,
